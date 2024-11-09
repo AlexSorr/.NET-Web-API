f1f7d0a848d21443adf7b267f2ed57daf1ecdc45
AlexSorr <alexsorr@yandex.ru>
2024-11-09 22:29:05 +0300

    clear_megrations script upd

--- TestWebAPI/clear_migrations.sh
+++ TestWebAPI/clear_migrations.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 migrations_path="$PWD"/Migrations
+migration_path=$PWD/Migrations
 
-echo -e "Migrations folder: $migrations_path"
-
-if [ ! -d "$migrations_path" ]; then
-    echo "Не найден каталог с миграциями по пути $migrations_path"
+# Проверка наличия папки Migrations
+if [ -d "$migration_path" ]; then
+    # Подсчёт количества видимых элементов в папке
+    count=$(find ./Migrations -maxdepth 1 -type f | grep -v "/\." | wc -l)
+    echo -e "$migration_path \nElements count: $count"
+else
+    echo "No migrations folder found"
     exit 1
 fi
 
+read -p "Continue (y/n): " choice
+
+if [[ "$choice" != "y" && "$choice" != "Y" ]]; then
+    echo "Abort"
+    exit 0
+fi
+
+echo "Deleting migrations"
+
 rm -rvf "$migrations_path"/*
+
+echo "Done"
