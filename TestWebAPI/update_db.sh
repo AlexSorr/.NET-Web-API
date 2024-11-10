c5f0c56fa1f43b35d918adb1316003879cd862dc
AlexSorr <alexsorr@yandex.ru>
2024-11-10 23:03:47 +0300

    helper sh-scripts upd

--- TestWebAPI/update_db.sh
+++ TestWebAPI/update_db.sh
@@ -11,7 +11,7 @@ migration_name="migration-$(date +'%Y-%m-%d_%H-%M-%S')"
 echo -e "Creating migration: $migration_name" 
 
 # Добавить миграцию, обновить БД
-dotnet ef migrations add "$migration_name" >&2
+dotnet ef migrations add "$migration_name" > /dev/null
 
 if [ $? -ne 0 ]; then
     echo "Creating migration error, abort"
